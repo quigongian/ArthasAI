0755fea73b0929d8490b372ca892a671a4db4438
David Moreno <davidmoreno1314@gmail.com>
2024-04-04 17:41:20 -0400

    dashboard now looks like figma design

--- frontend/arthas-ai/app/components/Navbar.tsx
+++ frontend/arthas-ai/app/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
 
 	return (
 		<>
-			<nav className="bg-white border-2 sticky top-0 z-50">
+			<nav className="bg-stone-200 border-2 sticky top-0 z-50">
 				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 					<div className="flex items-center justify-between h-16">
 						<div className="flex items-center">
