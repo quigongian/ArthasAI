50753d893ce5e3804c92fb5fcd1990b5592f078e
Jacob Schuster <jschuster8765@gmail.com>
2024-04-05 16:49:19 -0400

    make vector similarity work

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -23,8 +23,7 @@ def search_results(query: str):
     """
     Return the top k papers based on the abstract cosine similarity
     """
-
-    res = top_k_abstract_query(query)
+    res = top_k_abstract_query(query=query, k=3)
     return {"message": "Papers retrieved successfully", "data": res}
 
 @app.post("/paper")
--- backend/backend/utils/api/search.py
+++ backend/backend/utils/api/search.py
@@ -1,18 +1,21 @@
 from utils.db.neo4j import driver
+from utils.embeddings import get_embeddings
 
-def top_k_abstract_query(query: str): 
+def top_k_abstract_query(query: str, k: int = 5): 
     """
     Get the top k papers based on the abstract cosine similatiry
     TODO: maybe handle filters here?
     """
 
+    query_embedding = get_embeddings([query], "togethercomputer/m2-bert-80M-8k-retrieval")[0]
+
     QUERY = """
     MATCH (p:Paper)
-    call db.index.vector.queryNodes('abstract_embedding_index', 3, p.abstract_embedding) YIELD node, score
+    call db.index.vector.queryNodes('abstract_embedding_index', $k, $query_embedding) YIELD node, score
     WITH node {.*, abstract_embedding: null}, score
     return {paper: node, score: score} as data
     """
 
     with driver.session() as session:
-        result = session.run(QUERY)
+        result = session.run(QUERY, query_embedding=query_embedding, k=k)
         return result.data()
