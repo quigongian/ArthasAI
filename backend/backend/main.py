7aea0d5edfc60c2b905c981ab792d6358bbf7009
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2024-04-10 15:23:07 -0400

    asdasd

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import dotenv
 import os
 from utils.api.workspace import create_workspace, add_paper_to_workspace, get_all_workspaces, get_workspace
@@ -10,6 +10,23 @@ import together
 
 dotenv.load_dotenv()
 together.api_key = os.getenv("TOGETHER_API_KEY")
+
+from pydantic import BaseModel
+import boto3
+import pickle  # For serialization
+from storage import save_to_storage, load_from_storage, load_embeddings_from_s3, save_embeddings_to_s3
+from config import Settings
+from typing import List
+from embeddings import get_embeddings
+from dependencies import get_s3_client, get_together_client
+ 
+settings = Settings()
+
+together_api_key = get_together_client
+aws_access_key_id = settings.aws_access_key_id
+aws_secret_access_key = settings.aws_secret_access_key
+s3_client = get_s3_client()
+client = together.Together()
 
 app = FastAPI()
 
@@ -67,6 +84,34 @@ def route_get_all_workspaces_for_user():
 def chat():
     return {"Chat": "GET Request"}
 
+@app.post("/chat/{user_id}/{paper_id}")
+def chat(user_id: str, paper_id: str, chat_input: ChatInput):
+    #load existing context, if any
+    try:
+        context = load_from_storage(user_id, paper_id)
+    except HTTPException as e:
+        context = {'chat_history': []}
+
+    
+    #updating context with new chat input and embeddings
+    
+    #Note: You'd also want to store the chat output here
+    new_chat_input = chat_input.text
+    embeddings = get_embeddings([new_chat_input], model='togethercomputer/m2-bert-80M-8k-retrieval')
+    context['chat_history'].append({'input':new_chat_input, 'embedding': embeddings})
+    #save updated context
+
+    concatenated_prompts = "\n".join([entry['input'] for entry in context['chat_history']]) + "\n" + new_chat_input
+
+    save_to_storage(user_id, paper_id, context)
+
+        # Here you would integrate with your AI model to get a response, using the embeddings as needed
+    ai_response = "Response from the AI model"  # Placeholder for AI model integration
+
+
+    return {"Chat": ai_response}
+
+
 @app.get("/graph")
 def get_graph():
     return {"Graph": "GET Request"}
@@ -74,5 +119,10 @@ def get_graph():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app)
+    import sys
+    import os
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+    from backend.config import settings   
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
 
